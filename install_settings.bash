3a1967a5dd43f1a6d7c394006abf2c42fef9e356
Weidong Sun <weidong.sun@xyzrobotics.ai>
2022-03-16 17:47:35 +0800

    Go

--- install_settings.bash
+++ install_settings.bash
@@ -30,3 +30,16 @@ grep -qxF 'alias vi="vim"' ${BASHRC_FILE} || echo 'alias vi="vim"' >> ${BASHRC_F
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 cp bill_configs.vim ~/.vim_runtime/my_configs.vim
+
+# Install starship
+wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+mkdir ~/.fonts
+mv FiraCode.zip ~/.fonts/
+cd ~/.fonts && unzip FiraCode.zip && rm FiraCode.zip && fc-cache -fv && cd -
+
+curl -sS https://starship.rs/install.sh | sh
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
+mkdir -p ~/.config
+# mkdir -p ~/.config && touch ~/.config/starship.toml
+
+
