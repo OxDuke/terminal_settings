a7b78c39ea83c56be3352dcaf21d3557516a0c03
Weidong Sun <me@weidongsun.com>
2023-05-25 13:18:30 +0800

    quotes.

--- install_settings.bash
+++ install_settings.bash
@@ -23,9 +23,9 @@ rm -rf gnome-terminal
 # tmux settings
 ###############
 # Install tpm
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-cp tmux.bill.conf ~/.tmux.conf
-tmux source ~/.tmux.conf
+git clone "https://github.com/tmux-plugins/tpm" "~/.tmux/plugins/tpm"
+cp "tmux.bill.conf" "~/.tmux.conf"
+# tmux source ~/.tmux.conf
 
 ##############
 # vim settings
@@ -34,26 +34,26 @@ BASHRC_FILE="~/.bashrc"
 # Add vi alias to bashrc if not added.
 grep -qxF 'alias vi="vim"' ${BASHRC_FILE} || echo 'alias vi="vim"' >> ${BASHRC_FILE}
 
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
-cp bill_configs.vim ~/.vim_runtime/my_configs.vim
+git clone --depth=1 "https://github.com/amix/vimrc.git" "~/.vim_runtime"
+sh "~/.vim_runtime/install_awesome_vimrc.sh"
+cp "bill_configs.vim" "~/.vim_runtime/my_configs.vim"
 
 # Install Vundle, vim's plugin manager
 # Ref: https://github.com/VundleVim/Vundle.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim_runtime/bundle/Vundle.vim
+git clone "https://github.com/VundleVim/Vundle.vim.git" "~/.vim_runtime/bundle/Vundle.vim"
 vim +PluginInstall +qall
 
 ##################
 # Install starship
 ##################
+sudo apt update && sudo apt install -y wget curl
 wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-mkdir ~/.fonts
-mv FiraCode.zip ~/.fonts/
-cd ~/.fonts && unzip FiraCode.zip && rm FiraCode.zip && fc-cache -fv && cd -
+mkdir "~/.fonts"
+mv FiraCode.zip "~/.fonts/"
+cd "~/.fonts" && unzip FiraCode.zip && rm FiraCode.zip && fc-cache -fv && cd -
 
 curl -sS https://starship.rs/install.sh | sh
-echo 'eval "$(starship init bash)"' >> ~/.bashrc
-# mkdir -p ~/.config
-mkdir -p ~/.config && touch ~/.config/starship.toml
+echo 'eval "$(starship init bash)"' >> "~/.bashrc"
+mkdir -p "~/.config" && touch "~/.config/starship.toml"
 
 
